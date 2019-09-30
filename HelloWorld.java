8d034faa55f82c5b5d1a7ff6b76dcc5fc165b56d
Manu Jadli <majadli2@in.ibm.com>
2019-09-30 12:07:50 +0530

    change 3

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {	
 			System.out.println("********************* trying webhook ***************************");
-			System.out.println("********************* Hello and welcome to The United States Of America ***************************");
+			System.out.println("********************* Hello Manu ***********************");
 	}
 
 }
